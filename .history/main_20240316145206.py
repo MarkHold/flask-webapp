545d9e3f77749636394affa2b2c097ec2f83eb0d
Markus Lykonhold <lykonhold@gmail.com>
2024-03-19 10:26:47 +0100

    Base template

---
+++ .history/main_20240316145206.py
@@ -0,0 +1,8 @@
+from website import create_app
+
+app = create_app()
+
+# The if __name__ == '__main__': block is used to check if the script is being run directly,
+if __name__ == '__main__':
+   # debug=True makes the server restart after everytime you save a change in the code.
+    app.run(debug=True)
